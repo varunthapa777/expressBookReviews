46abfe261a52d071663a6eeb15368d7f084ddfb6
varunthapa777 <varunthapa1920@gmail.com>
2024-08-07 20:14:10 +0530

    completed all task

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,41 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({ secret: "secretKey", resave: true, saveUninitialized: true })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  // Check if the user has an access token in the session
+  if (!req.session.accessToken) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  // Verify the access token and extract the user password from it
+  const accessToken = req.session.accessToken;
+  try {
+    const decodedToken = jwt.verify(accessToken, "secretKey");
+    const userPassword = decodedToken.userPassword;
+    req.userPassword = userPassword;
+    next();
+  } catch (err) {
+    return res.status(401).json(err);
+
+    //return res.status(401).json({accessToken:req.session.accessToken, message: "Invalid access token" });
+  }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
